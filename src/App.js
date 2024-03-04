ab597b2d0c1a20d8a3a2f5093df01fc4370e88cf
TabindaNoor <tabindanoor415@gmail.com>
2024-03-04 18:15:05 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import PaymentForm from './PaymentForm';
+
+const stripePromise = loadStripe('your_publishable_key_here');
+
+const App = () => {
+  return (
+    <Elements stripe={stripePromise}>
+      <PaymentForm />
+      <p>this is p </p>
+    </Elements>
+  );
+};
+
+export default App;

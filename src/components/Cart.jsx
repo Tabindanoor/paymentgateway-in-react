7e963f1c82728abf8d34b11bb1107bf7f9bf4e69
TabindaNoor <tabindanoor415@gmail.com>
2024-03-14 14:25:17 +0500

    working

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,9 +7,18 @@ import { useCart } from './CartContext';
 const Cart = () => {
   const { state, dispatch } = useCart();
 
+  // const handleRemoveItem = (itemId, price, quantity) => {
+  //   dispatch({ type: 'REMOVE_ITEM', payload: { id: itemId, price, quantity } });
+  // };
+
   const handleRemoveItem = (itemId, price, quantity) => {
-    dispatch({ type: 'REMOVE_ITEM', payload: { id: itemId, price, quantity } });
+    if (quantity > 1) {
+      dispatch({ type: 'UPDATE_QUANTITY', payload: { id: itemId, quantity: quantity - 1 } });
+    } else {
+      dispatch({ type: 'REMOVE_ITEM', payload: { id: itemId, price, quantity } });
+    }
   };
+  
 
   return (
     <div>
--- src/components/CartContext.jsx
+++ src/components/CartContext.jsx
@@ -1,3 +1,46 @@
+// import React, { createContext, useContext, useReducer } from 'react';
+
+// const CartContext = createContext();
+
+// const initialState = {
+//   items: [],
+//   totalPrice: 0,
+// };
+
+// const cartReducer = (state, action) => {
+//   switch (action.type) {
+//     case 'ADD_ITEM':
+//       const existingIndex = state.items.findIndex(item => item.id === action.payload.id);
+//       if (existingIndex !== -1) {
+//         const updatedItems = [...state.items];
+//         updatedItems[existingIndex].quantity++;
+//         return {
+//           ...state,
+//           items: updatedItems,
+//           totalPrice: state.totalPrice + action.payload.price,
+//         };
+//       } else {
+//         return {
+//           ...state,
+//           items: [...state.items, { ...action.payload, quantity: 1 }],
+//           totalPrice: state.totalPrice + action.payload.price,
+//         };
+//       }
+//     case 'REMOVE_ITEM':
+//       const itemToRemove = state.items.find(item => item.id === action.payload.id);
+//       const updatedItems = state.items.filter(item => item.id !== action.payload.id);
+//       return {
+//         ...state,
+//         items: updatedItems,
+//         totalPrice: state.totalPrice - (itemToRemove.price * itemToRemove.quantity),
+//       };
+      
+//     default:
+//       return state;
+//   }
+// };
+
+
 import React, { createContext, useContext, useReducer } from 'react';
 
 const CartContext = createContext();
@@ -6,6 +49,7 @@ const initialState = {
   items: [],
   totalPrice: 0,
 };
+
 
 const cartReducer = (state, action) => {
   switch (action.type) {
@@ -28,16 +72,29 @@ const cartReducer = (state, action) => {
       }
     case 'REMOVE_ITEM':
       const itemToRemove = state.items.find(item => item.id === action.payload.id);
-      const updatedItems = state.items.filter(item => item.id !== action.payload.id);
+      const updatedItemsRemove = state.items.filter(item => item.id !== action.payload.id);
       return {
         ...state,
-        items: updatedItems,
+        items: updatedItemsRemove,
         totalPrice: state.totalPrice - (itemToRemove.price * itemToRemove.quantity),
       };
+
+    case 'UPDATE_QUANTITY':
+      const itemToUpdate = state.items.find(item => item.id === action.payload.id);
+      const updatedItemsQuantity = state.items.map(item =>
+        item.id === action.payload.id ? { ...item, quantity: action.payload.quantity } : item
+      );
+      return {
+        ...state,
+        items: updatedItemsQuantity,
+        totalPrice: state.totalPrice + (action.payload.quantity - itemToUpdate.quantity) * itemToUpdate.price,
+      };
+
     default:
       return state;
   }
 };
+
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, initialState);
